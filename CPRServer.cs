371cad85bb613efaf5d92a94fea2e6fac17e1108
James <42484571+smitteh1@users.noreply.github.com>
2018-11-10 22:26:48 -0500

    Update CPRServer.cs

--- CPRServer.cs
+++ CPRServer.cs
@@ -1,1 +1,13 @@
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core;
 
+namespace CPR.Server
+{
+    public class ServerScript : BaseScript
+    {
+        public ServerScript()
+        {
+            // constructor code
+        }
+    }
+}
